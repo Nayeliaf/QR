48beddec41bf18768b8b5787c720d349d273fc6b
Nayeliaf <npaf31@hotmail.com>
2026-03-06 02:15:51 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = {
   eventName: "AKUAI",
   subtitle: "Escaneo · Registro de entrada",
   version: "1.0",
-  apiUrl: "https://script.google.com/macros/s/AKfycbz-JWHEGu4M6sHAbV1LF4WSs3AqlJnGzTkCYJFq-aInYKyqgEyeBHYhndEiQwAR4nPv/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwbfvhnIJu7XM2kFr1_AmaXni1V233I4dVuBA9W59PXhIOQPu-abi7e35ZLQsFPNlQW/exechttps://script.google.com/macros/s/AKfycbwIzZaPi2OeJRMfmIvcBK72cgHaCPc6C05CKKZkkFu5GkvP83c0RkpGTUBDC459SbWRXg/exec",
   useMock: false
 };
